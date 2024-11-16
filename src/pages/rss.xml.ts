550d18f97bad8f6370cd2d83f15a97c782592dfd
Kasia Biernat <kasia.biernat@hyland.com>
2024-11-16 14:44:11 +0100

    fix rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,24 +3,22 @@ import { getCollection } from "astro:content";
 import { PAGES } from "@consts";
 
 type Context = {
-  site: string
-}
+  site: string;
+};
 
 export async function GET(context: Context) {
   const blog = (await getCollection("blog"))
-  .filter(post => !post.data.draft);
-
-  const talks = (await getCollection("talks"))
-    .filter(project => !project.data.draft);
-
-  const items = [...blog, ...talks]
-    .sort((a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf());
+    .filter((post) => !post.data.draft)
+    .sort(
+      (a, b) =>
+        new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf()
+    );
 
   return rss({
     title: PAGES.HOME.TITLE,
     description: PAGES.HOME.DESCRIPTION,
     site: context.site,
-    items: items.map((item) => ({
+    items: blog.map((item) => ({
       title: item.data.title,
       description: item.data.description,
       pubDate: item.data.date,
